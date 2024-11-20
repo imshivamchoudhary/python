c39d795227b5c0c09bd51563bbab65226285789e
imshivamchoudhary <shivamchoudhary012@gmail.com>
2024-11-20 23:11:47 +0530

    how to use comment etc in python

---
+++ comments,escapes,sequences.py
@@ -0,0 +1,9 @@
+print('hey i am a good boy \nand my brother is also a good boy ')
+# \n --> it is used for go to next line
+# \n --> is a escape sequence character
+# Escape sequence character
+# to insert character that cannot be directly used in string we use an escape sequence character
+# an escaped sequenced character is a backslash (\) followed by the character you want to insert
+print("im shivam choudhary\ni am a \"good boy\"")
+print('shivam',18,23,sep='=',end=' 001\n')
+print('shivam')
