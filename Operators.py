d95e839e2574c167246cc9e43b513acc06da1a0f
imshivamchoudhary <shivamchoudhary012@gmail.com>
2024-11-20 23:52:13 +0530

    operators use in python

---
+++ Operators.py
@@ -0,0 +1,18 @@
+# python has different types of operators for different operations.
+# 1 Arithmetic operators
+#+,-,*,/
+# // --> it is used to calculate floor divisin value
+# % --> it is modules it used to find the remainder of the value
+# ** --> it is a power
+a=2
+b=6
+list1=[a+b,a-b,a/b,a*b,a//b,a%b,b**a]
+print(list1)
+
+# Question
+x=int(input('Enter the value of x: '))
+y=int(input('Entre the value of y: '))
+print('addtion of a+b',a+b)
+print('subtraction of a-b',a-b)
+print('multiplication of a*b',a*b)
+print('division of a/b',a/b)
